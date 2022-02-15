cea2a93d24f6d39a87aceb7b775041bbca3f9c2a
Luis Guiraldelli <guirald@pop-os.localdomain>
2022-02-15 18:51:30 -0300

    feat: improving project

--- src/tweets/check-new-tweets/check-new-tweets.task.ts
+++ src/tweets/check-new-tweets/check-new-tweets.task.ts
@@ -7,39 +7,38 @@ import { TweetsService } from '../tweets.service';
 
 @Injectable()
 export class CheckNewTweetsTask {
+  private limit = 10;
 
-    private limit = 10;
+  constructor(
+    private tweetService: TweetsService,
+    @Inject(CACHE_MANAGER)
+    private cache: Cache,
+    @InjectQueue('emails')
+    private emailsQueue: Queue,
+  ) {}
 
-    constructor(private tweetService: TweetsService,
-        @Inject(CACHE_MANAGER)
-        private cache: Cache,
-        @InjectQueue('emails')
-        private emailsQueue: Queue,
-    ) {}
+  @Interval(5000)
+  async handle() {
+    console.log('procurando tweets...');
+    let offset = await this.cache.get<number>('tweet-offset');
+    offset = offset === undefined || offset === null ? 0 : offset;
+    console.log(`offset: ${offset}`);
 
-    @Interval(5000)
-    async handle() {
-        console.log('procurando tweets...');
-        let offset = await this.cache.get<number>('tweet-offset');
-        offset = offset === undefined || offset === null ? 0 : offset;
-        console.log(`offset: ${offset}`);
+    const tweets = await this.tweetService.findAll({
+      offset,
+      limit: this.limit,
+    });
 
-        const tweets = await this.tweetService.findAll({
-            offset,
-            limit: this.limit,
-        });
+    console.log(`tweets count: ${tweets.length}`);
 
-        console.log(`tweets count: ${tweets.length}`);
+    if (tweets.length === this.limit) {
+      console.log('achou mais tweets');
 
-        if (tweets.length === this.limit) {
-            console.log('achou mais tweets');
+      await this.cache.set('tweet-offset', offset + this.limit, {
+        ttl: 1 * 60 * 10,
+      });
 
-            await this.cache.set('tweet-offset', offset + this.limit, {
-                ttl: 1 * 60 * 10,
-            });
-
-            this.emailsQueue.add({});
-        }
+      this.emailsQueue.add({});
     }
-
+  }
 }
--- src/tweets/tweets.module.ts
+++ src/tweets/tweets.module.ts
@@ -16,6 +16,7 @@ import { ConfigModule } from '@nestjs/config';
         store: redisStore,
         host: process.env.REDIS_HOST,
         port: parseInt(process.env.REDIS_PORT),
+        auth_pass: process.env.REDIS_PASSWORD,
       }),
     }),
     MongooseModule.forFeature([
